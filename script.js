5472e62ad0ffbca39c7494e05ec017cc80825e97
Paulo Zeredo <82038781+paulozeredo@users.noreply.github.com>
2024-02-25 19:10:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function adicionarTarefa() {
+    var tarefa = document.getElementById('tarefaInput').value;
+    if (tarefa) {
+        var tabela = document.getElementById('tabelaTarefas');
+        var linha = tabela.insertRow(-1);
+        var celula1 = linha.insertCell(0);
+        var celula2 = linha.insertCell(1);
+        var celula3 = linha.insertCell(2);
+
+        /* Cada tarefa tem seu próprio botão de exclusão representado por um ícone de lixeira.
+        Quando o botão de exclusão é pressionado, a tarefa associada é excluída.
+        Quando a caixa de seleção é marcada, o texto da tarefa é tachado, usando CSS.
+        Quando a caixa de seleção é desmarcada, o texto da tarefa volta ao normal. */
+        celula1.innerHTML = '<input type="checkbox" onchange="tacharTarefa(this)">';
+        celula2.innerHTML = tarefa; // Aqui usamos a tarefa na forma como foi digitada
+        celula3.innerHTML = '<button onclick="excluirTarefa(this)">🗑️</button>';
+
+        document.getElementById('tarefaInput').value = '';
+    }
+}
+
+function excluirTarefa(botao) {
+    var linha = botao.parentNode.parentNode;
+    linha.parentNode.removeChild(linha);
+}
+
+/* Quando a caixa de seleção é marcada, o texto da tarefa é tachado, usando CSS.
+Quando a caixa de seleção é desmarcada, o texto da tarefa volta ao normal. */
+function tacharTarefa(checkbox) {
+    var celulaTarefa = checkbox.parentNode.nextSibling;
+    if (checkbox.checked) {
+        celulaTarefa.classList.add('tarefa-concluida');
+    } else {
+        celulaTarefa.classList.remove('tarefa-concluida');
+    }
+}
